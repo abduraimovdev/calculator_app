c22e0aa7198fb9ec884629a81d0c3b3cf2dbbf27
abduraimovdev <kenzo778118@gmail.com>
2023-02-28 00:04:29 +0500

    updated _>

--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,4 @@
 import 'package:calculator_app/pages/intro_page.dart';
-import 'package:calculator_app/services/io_service.dart';
-import 'package:calculator_app/models/arithmetic_calculator_model.dart';
-import 'dart:math';
 void main() {
   IntroPage();
 }
