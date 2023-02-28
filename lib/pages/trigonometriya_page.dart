d7a90f136eb8c4691b8f9d166b90b2218ec8cb58
Usmanov Mansurjon <usmanovmansur@gmail.com>
2023-02-28 08:40:07 +0500

    trigonometriya model trigonometriya model1 trigonometriya page created

---
+++ lib/pages/trigonometriya_page.dart
@@ -0,0 +1,31 @@
+import '../models/trigonometriya_model1.dart';
+import '../services/io_service.dart';
+
+class Trigometr {
+  Trigometr() {
+    print("Burchak gradus o'lchodda beriladi.");
+    print("Masalan: sinus40");
+    print("Masalan: cosinus40");
+    print("Masalan: tangens40");
+    print("Masalan: cotangens40");
+    trigonom();
+  }
+
+  void trigonom() {
+    String text7 = io.text;
+    trigonometry(text7);
+    print("1.Trionometriyani hisobllang");
+    print("2.Bosh menyuga qaytish");
+    print("3.Exit");
+    int numb2 = io.number;
+    if (numb2 == 1) {
+      trigonom();
+    }
+    else if (numb2 == 2) {
+      trigonom();//
+    }
+    else {
+      trigonom();
+    }
+  }
+}
